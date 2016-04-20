635966302e0b90c34814c7b0b9c9b7763d083d10
julia sjögren <Juliasjogren@live.se>
2016-04-20 16:43:18 +0200

    Init
    Hej

---
+++ Game/Assets/ScrollingBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float speed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * speed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
