115d6e160eda11bd0bf6349f34647e79856ba43b
Poliky4 <poliky44@hotmail.com>
2016-04-27 10:45:40 +0200

    e

--- Game/Assets/Scripts/MeteroidHandler.cs
+++ Game/Assets/Scripts/MeteroidHandler.cs
@@ -3,9 +3,8 @@ using System.Collections;
 
 public class MeteroidHandler : MonoBehaviour
 {
-
     private Vector3 startPosition;
-    public GameObject meteroid;
+    public GameObject meteroid, warningIndicator;
     private float nextSpawn = 2.0f;
     public float spawnRate;
 
@@ -22,9 +21,10 @@ public class MeteroidHandler : MonoBehaviour
         if (Time.time > nextSpawn)
         {
             nextSpawn = Time.time + spawnRate;
-            startPosition = new Vector3(Random.Range(-1100.0f, 1100.0f), 560);
-
+            startPosition = new Vector3(Random.Range(-1100.0f, 1100.0f), 700);
             Instantiate(meteroid, startPosition, Quaternion.identity);
+            startPosition.y = 591.4f;
+            Instantiate(warningIndicator, startPosition, Quaternion.identity);
         }
     }
 }
---
+++ Game/Assets/Scripts/WarningIndicator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarningIndicator : MonoBehaviour
+{
+    private int warnTime = 4; //seconds
+    private float timer;
+    // Use this for initialization
+    void Start()
+    {
+        timer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer + warnTime < Time.time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
